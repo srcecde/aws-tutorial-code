4055f25ed95b995249ed31771d334171288311fe
Srce Cde <chiragr83@gmail.com>
2018-07-19 15:27:18 +0530

    comprehend-syntax-analysis

---
+++ lambda/lambda_comprehend_syntax_analysis.py
@@ -0,0 +1,21 @@
+"""
+-*- coding: utf-8 -*-
+========================
+AWS Lambda
+========================
+Contributor: Chirag Rathod (Srce Cde)
+========================
+"""
+
+import boto3
+from pprint import pprint
+def lambda_handler(event, context):
+
+    comprehend = boto3.client("comprehend")
+    paragraph = "Hey, welcome to this tutorial. How are you doing readers and viewers?"
+
+    #Extracting Grammer syntax
+    syntax_response = comprehend.detect_syntax(Text = paragraph, LanguageCode = "en")
+    pprint(syntax_response)
+
+    return 'Thanks'
