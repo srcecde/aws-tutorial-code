f6f71c7b3d31edb18784b7e7738be03c17f60d0e
Srce Cde <chiragr83@gmail.com>
2018-08-06 19:18:20 +0530

    code-fix

--- lambda/lambda_cp_buc_rn_obuc.py
+++ lambda/lambda_cp_buc_rn_obuc.py
@@ -11,11 +11,11 @@ import boto3
 from pprint import pprint
 def lambda_handler(event, context):
     s3 = boto3.resource("s3")
-    source_bucket = "sbucket-name"
-    destination_bucket = "dbucket-name"
+    source_bucket = s3.Bucket("jugad")
+    destination_bucket = s3.Bucket("blaze-text")
 
     for obj in source_bucket.objects.all():
         destination_bucket_file_rename = obj.key + str("_new")
-        s3.Object(dest_b.name,dest_filename).copy_from(CopySource= { 'Bucket': obj.bucket_name , 'Key' : obj.key})
+        s3.Object(destination_bucket.name, destination_bucket_file_rename).copy_from(CopySource= { 'Bucket': obj.bucket_name , 'Key' : obj.key})
     
     return 'Thanks'
