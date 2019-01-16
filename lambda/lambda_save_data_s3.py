a4160db622a14fd9548b4f8993d7ade8a765e029
Srce Cde <18462032+srcecde@users.noreply.github.com>
2019-01-16 11:31:46 +0530

    lambda-upload-data-s3-without-save-file

---
+++ lambda/lambda_save_data_s3.py
@@ -0,0 +1,26 @@
+"""
+-*- coding: utf-8 -*-
+========================
+AWS Lambda
+========================
+Contributor: Chirag Rathod (Srce Cde)
+========================
+"""
+
+# importing AWS SDK for Python
+import boto3
+
+
+def lambda_handler(event, context):
+	"""Lambda function"""
+    client = boto3.client("ec2")
+    s3 = boto3.client("s3")
+
+    # fetching EC2 instance state and status check information
+    status = client.describe_instance_status(IncludeAllInstances = True)
+    
+    # Writing data to S3 bucket (text file)
+    s3.put_object(Bucket = "bucket-name", Key = "data-log.txt", Body = str(status))
+
+    # for ex. while dealing with json or any other file type, need to pass the ContentType parameter
+    # s3.put_object(Bucket = "bucket-name", Key = "data-log.json", Body = str(status), ContentType = "application/json")
