db21322ee5efeae5536dc59e493fdc9c27a9306a
Srce Cde <chiragr83@gmail.com>
2019-04-06 10:54:45 +0530

    image-process-opencv-lambda

---
+++ lambda/lambda_image_process_opencv.py
@@ -0,0 +1,28 @@
+"""
+-*- coding: utf-8 -*-
+========================
+AWS Lambda
+========================
+Contributor: Chirag Rathod (Srce Cde)
+========================
+"""
+import cv2
+import numpy as np
+import boto3
+
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    bucket_name = "bucket_name"
+    file_obj = s3.get_object(Bucket=bucket_name, Key="obj_scene.jpeg")
+    file_content = file_obj["Body"].read()
+
+    np_array = np.fromstring(file_content, np.uint8)
+    image_np = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("/tmp/gray_obj.jpg", gray)
+
+    s3.put_object(Bucket=bucket_name, Key="grayscale.jpg", Body=open("/tmp/gray_obj.jpg", "rb").read())
+
+    return "Thanks"
