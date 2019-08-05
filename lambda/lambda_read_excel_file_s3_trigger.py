41b2b96690c567d92fdfa610b6f2bfa2cab34414
Srce Cde <chiragr83@gmail.com>
2019-08-05 08:31:18 +0530

    read-excel-file

---
+++ lambda/lambda_read_excel_file_s3_trigger.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+__author__ = "Chirag Rathod (Srce Cde)"
+__license__ = "MIT"
+__email__ = "chiragr83@gmail.com"
+__maintainer__ = "Chirag Rathod (Srce Cde)"
+
+
+import json
+import boto3
+import pandas as pd
+import io
+
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    s3_resource = boto3.resource("s3")
+    if event:
+        s3_records = event["Records"][0]
+        bucket_name = str(s3_records["s3"]["bucket"]["name"])
+        file_name = str(s3_records["s3"]["object"]["key"])
+        file_obj = s3.get_object(Bucket=bucket_name, Key=file_name)
+        file_content = file_obj["Body"].read()
+
+        read_excel_data = io.BytesIO(file_content)
+
+        df = pd.read_excel(read_excel_data)
+        df = df.assign(dummy="dummy_value")
+        df.to_csv("/tmp/updated.csv")
+        
+        s3_resource.Bucket("bucket-name").upload_file("/tmp/updated.csv", "updated.csv")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
