03f971503361b7d59461738067ac678956306c4e
Srce Cde <chiragr83@gmail.com>
2020-01-04 17:03:52 +0530

    lambda-api-upload-binary-file

---
+++ lambda/lambda_api_upload_binary.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+__author__ = "Chirag Rathod (Srce Cde)"
+__license__ = "MIT"
+__email__ = "chiragr83@gmail.com"
+__maintainer__ = "Chirag Rathod (Srce Cde)"
+
+import json
+import base64
+import boto3
+
+def lambda_handler(event, context):
+	s3 = boto3.client("s3")
+
+	# retrieving data from event
+	get_file_content = event["content"]
+
+	# decoding data
+	decode_content = base64.b64decode(get_file_content)
+
+	# uploading file to S3 bucket
+	s3_upload = s3.put_object(Bucket="bucket-name", Key="file.pdf", Body=decode_content)
+
+	return {
+        "statusCode": 200,
+        "body": json.dumps("Thanks for using!")
+        }
