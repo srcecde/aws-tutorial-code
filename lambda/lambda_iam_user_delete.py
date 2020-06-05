76af34e0757cbc9a42a9ab2adfb1353771604253
srcecde <chiragr83@gmail.com>
2020-06-05 18:16:07 +0530

    lambda-delete-user-lastaccessed

---
+++ lambda/lambda_iam_user_delete.py
@@ -0,0 +1,30 @@
+"""
+-*- coding: utf-8 -*-
+========================
+AWS Lambda
+========================
+Contributor: Chirag Rathod (Srce Cde)
+========================
+"""
+
+import json
+import boto3
+import datetime
+
+def lambda_handler(event, context):
+    # delete IAM user based on password last used
+
+    client = boto3.client("iam")
+    today = datetime.datetime.now()
+    data = client.list_users()
+    
+    for i in data["Users"]:
+        days = (today - i["PasswordLastUsed"].replace(tzinfo=None)).days
+        username = i["UserName"]
+        if days > 60:
+            client.delete_user(UserName=username)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Thanks!')
+    }
