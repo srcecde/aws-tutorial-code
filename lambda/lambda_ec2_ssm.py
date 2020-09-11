478860ecdf9ce702056b41a830a61a10e1f20c30
Srce Cde <chiragr83@gmail.com>
2020-09-11 23:21:48 +0530

    lambda-ec2-ssm-comment-fix

--- lambda/lambda_ec2_ssm.py
+++ lambda/lambda_ec2_ssm.py
@@ -18,22 +18,27 @@ def lambda_handler(event, context):
     describeInstance = client.describe_instances()
 
     InstanceId=[]
-    # fetchin public IP address of the running instances
+    # fetchin instance id of the running instances
     for i in describeInstance['Reservations']:
         for instance in i['Instances']:
             if instance["State"]["Name"] == "running":
                 InstanceId.append(instance['InstanceId'])
 
+    # looping through instance ids
     for instanceid in InstanceId:
+        # command to be executed on instance
         response = ssm.send_command(
                 InstanceIds=[instanceid],
                 DocumentName="AWS-RunShellScript",
-                Parameters={'commands': ['command_to_be_executed']}, )
+                Parameters={'commands': ['command_to_be_executed']} # replace command_to_be_executed with command
+                )
 
+        # fetching command id for the output
         command_id = response['Command']['CommandId']
         
         time.sleep(3)
         
+        # fetching command output
         output = ssm.get_command_invocation(
               CommandId=command_id,
               InstanceId=instanceid
