dc830af3e3f8fbf7fe684f05b95f4feec9da1b43
Srce Cde <chiragr83@gmail.com>
2021-10-23 13:43:12 +0530

    textract-async-add-logging-exception-handling

--- textract/async-form-table/lambda-process-response/helper/helper.py
+++ textract/async-form-table/lambda-process-response/helper/helper.py
@@ -7,17 +7,37 @@ Contributor: Chirag Rathod (Srce Cde)
 ========================
 """
 
+import sys
 import boto3
 import io
+import logging
+import traceback
+import json
 from .parser import Parse
 import pandas as pd
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def process_error():
+    ex_type, ex_value, ex_traceback = sys.exc_info()
+    traceback_string = traceback.format_exception(ex_type, ex_value, ex_traceback)
+    error_msg = json.dumps(
+        {
+            "errorType": ex_type.__name__,
+            "errorMessage": str(ex_value),
+            "stackTrace": traceback_string,
+        }
+    )
+    return error_msg
 
 
 def upload_to_s3(csv_buffer, BUCKET_NAME, key):
-    client = boto3.client('s3')
+    client = boto3.client("s3")
     client.put_object(Body=csv_buffer.getvalue(), Bucket=BUCKET_NAME, Key=key)
-    
+
+
 def extract_kv(final_map):
     keys, values = [], []
     for i in final_map:
@@ -26,69 +46,78 @@ def extract_kv(final_map):
             values.append(v)
     return keys, values
 
+
 def save_kv_csv(keys, values, job_id, BUCKET_NAME):
-    key = f'kv/{job_id}/key_value.csv'
+    key = f"kv/{job_id}/key_value.csv"
     df = pd.DataFrame()
-    df['Keys'] = keys
-    df['Values'] = values
+    df["Keys"] = keys
+    df["Values"] = values
     csv_buffer = io.StringIO()
     df.to_csv(csv_buffer)
     upload_to_s3(csv_buffer, BUCKET_NAME, key)
-    
+
+
 def extract_kv_text(text):
     keys, values = [], []
     for k, v in text.items():
-            keys.append(k)
-            values.append(v)
+        keys.append(k)
+        values.append(v)
     return keys, values
+
 
 def save_table_csv(table, job_id, BUCKET_NAME):
     for i, j in table.items():
         for k, v in j.items():
             csv_buffer = io.StringIO()
             df = pd.DataFrame(v)
-            df.columns = df.iloc[0] 
+            df.columns = df.iloc[0]
             df = df[1:]
             df.to_csv(csv_buffer)
-            key = f'tables/{job_id}/{k}.csv'
+            key = f"tables/{job_id}/{k}.csv"
             upload_to_s3(csv_buffer, BUCKET_NAME, key)
 
+
 def save_text_csv(keys, values, job_id, BUCKET_NAME):
-    key = f'text/{job_id}/text.csv'
+    key = f"text/{job_id}/text.csv"
     df = pd.DataFrame()
-    df['PageNo'] = keys
-    df['Text'] = values
+    df["PageNo"] = keys
+    df["Text"] = values
     csv_buffer = io.StringIO()
     df.to_csv(csv_buffer)
     upload_to_s3(csv_buffer, BUCKET_NAME, key)
 
+
 def process_response(BUCKET_NAME, job_id, get_table=True, get_kv=True, get_text=True):
-    textract = boto3.client('textract')
-    
+    textract = boto3.client("textract")
+
     response = {}
     pages = []
-    
+
+    logging.info("Fetching response")
     response = textract.get_document_analysis(JobId=job_id)
-    
+
     pages.append(response)
 
     nextToken = None
-    if('NextToken' in response):
-        nextToken = response['NextToken']
+    logging.info("Checking paginated response")
+    if "NextToken" in response:
+        logging.info("Paginated response found")
+        nextToken = response["NextToken"]
 
-    while(nextToken):
+    while nextToken:
         response = textract.get_document_analysis(JobId=job_id, NextToken=nextToken)
         pages.append(response)
         nextToken = None
-        if('NextToken' in response):
-            nextToken = response['NextToken']
-    
+        if "NextToken" in response:
+            nextToken = response["NextToken"]
+
     keys, values = [], []
     text_key, text_value = [], []
+    logger.info("Looping through pages & parsing the response")
     for page in pages:
         parse = Parse(page=page, get_table=True, get_kv=True, get_text=True)
         table, final_map, text = parse.process_response()
-        
+
         if get_table:
             save_table_csv(table, job_id, BUCKET_NAME)
 
@@ -105,3 +134,4 @@ def process_response(BUCKET_NAME, job_id, get_table=True, get_kv=True, get_text=
         save_kv_csv(keys, values, job_id, BUCKET_NAME)
     if get_text:
         save_text_csv(text_key, text_value, job_id, BUCKET_NAME)
+    logger.info("Parsing completed")
--- textract/async-form-table/lambda-process-response/lambda_function.py
+++ textract/async-form-table/lambda-process-response/lambda_function.py
@@ -9,16 +9,39 @@ Contributor: Chirag Rathod (Srce Cde)
 
 import os
 import json
-from helper.helper import process_response
+import logging
+from helper.helper import process_response, process_error
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    BUCKET_NAME = os.environ["BUCKET_NAME"]
+    logger.info(f"Destination bucket: {BUCKET_NAME}")
+except Exception as e:
+    error_msg = process_error()
+    logger.error(error_msg)
 
 
 def lambda_handler(event, context):
+    try:
+        if "Records" in event:
+            logger.info(f"Event: {event}")
+            job_id = json.loads(event["Records"][0]["Sns"]["Message"])["JobId"]
+            logger.info(f"JobId: {job_id}")
 
-    BUCKET_NAME = os.environ["BUCKET_NAME"]
+            logger.info("Parsing initiated")
+            process_response(
+                BUCKET_NAME, job_id, get_table=True, get_kv=True, get_text=True
+            )
 
-    job_id = json.loads(event["Records"][0]["Sns"]["Message"])["JobId"]
-    print(job_id)
+            logger.info("CSV files uploaded to S3")
 
-    process_response(BUCKET_NAME, job_id, get_table=True, get_kv=True, get_text=True)
-
-    return {"statusCode": 200, "body": json.dumps("File uploaded successfully!")}
+            return {
+                "statusCode": 200,
+                "body": json.dumps("File uploaded successfully!"),
+            }
+    except Exception as e:
+        error_msg = process_error()
+        logger.error(error_msg)
+        return {"statusCode": 500, "body": json.dumps("Error parsing the response!")}
