2938ec1d30e75adf231c81d83e8119bfe6806833
Srce Cde <chiragr83@gmail.com>
2021-11-03 21:43:04 +0530

    removed-dummy-defaults-config

--- textract/bounding-box-kv-tables/flask-app/config.py
+++ textract/bounding-box-kv-tables/flask-app/config.py
@@ -4,20 +4,20 @@ import os
 class Config:
     @property
     def AWS_ACCESS_KEY_ID(self):
-        return os.environ.get("AWS_ACCESS_KEY_ID", "AKIAQYQ5LM66HKWAOCUI")
+        return os.environ.get("AWS_ACCESS_KEY_ID", "")
 
     @property
     def AWS_SECRET_ACCESS_KEY(self):
         return os.environ.get(
-            "AWS_SECRET_ACCESS_KEY", "IaUKNlrcF5A/izfC4B7TSmqR/jzX6dWSFD60aPv6"
+            "AWS_SECRET_ACCESS_KEY", ""
         )
 
     @property
     def HOST(self):
-        return os.environ.get("HOST", "v80579pkii.execute-api.us-east-1.amazonaws.com")
+        return os.environ.get("HOST", "")
 
     @property
     def ENDPOINT(self):
         return os.environ.get(
-            "ENDPOINT", "https://v80579pkii.execute-api.us-east-1.amazonaws.com/v1"
+            "ENDPOINT", ""
         )
